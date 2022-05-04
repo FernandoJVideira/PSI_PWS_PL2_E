9de0bfcdbc75f4819baa96c1d7f6c92b839d5623
Diogo <tbpt30@hotmail.com>
2022-05-04 18:13:50 +0100

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 8d2b1246f31dcd2c0c6d5a45d6ec1e1cca90b253.

--- views/home/index.php
+++
@@ -1,8 +0,0 @@
-
-    <section class="foco-pagina">
-        <div class="container">
-            <img src="public\img\dummy_700x300_000000_55acdb.png" alt="web">
-            <h1>Bem-vindo ao nosso site web!</h1>
-            <h5>Esta é apenas uma pequena amostra</h5>
-        </div>
-    </section>
