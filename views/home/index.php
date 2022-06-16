1bf78ed6ba056ad20769babac89045a7620d31df
FernandoJVideira <2211877@my.ipleiria.pt>
2022-06-16 21:52:12 +0100

    Auto stash before merge of "FaturaController_BugFixes" and "origin/FaturaController_BugFixes"

--- views/home/index.php
+++ views/home/index.php
@@ -1,7 +1,9 @@
 <section class="foco-pagina">
     <div class="container">
         <img src="public\img\dummy_700x300_000000_55acdb.png" alt="web">
-        <h1>Bem-vindo ao nosso site web!</h1>
-        <h5>Esta é apenas uma pequena amostra</h5>
+        <h1>Bem-Vindo a Fatura+</h1>
+        <h5>Um pouco sobre nós:</h5>
+        <br>
+        <p>Fatura +</p>
     </div>
 </section>
