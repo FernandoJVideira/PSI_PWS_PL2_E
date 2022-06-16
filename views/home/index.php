dace3d56f76d7f24ea5ba66b1cb437dc8131be1e
FernandoJVideira <2211877@my.ipleiria.pt>
2022-06-16 23:30:42 +0100

    HomePage Update e Connection String Fixed for the 1000th time

--- views/home/index.php
+++ views/home/index.php
@@ -1,7 +1,10 @@
 <section class="foco-pagina">
     <div class="container">
-        <img src="public\img\dummy_700x300_000000_55acdb.png" alt="web">
-        <h1>Bem-vindo ao nosso site web!</h1>
-        <h5>Esta é apenas uma pequena amostra</h5>
+        <img src="public\img\yes2.png" alt="web">
+        <h1>Bem-Vindo a Fatura+</h1>
+        <h5>Um pouco sobre o projeto:</h5>
+        <br>
+        <p>Fatura+ é um projeto desenvolvido no âmbito da disciplina de Programação Web-Servidor cujo objetivo é a criação, 
+            gestão e acesso dos dados de faturas de múltiplos clientes de uma determinada empresa.</p>
     </div>
 </section>
