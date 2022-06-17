9d9230048d6048091073b910a442474ed0af8009
marcohbzk <95863761+marcohbzk@users.noreply.github.com>
2022-06-17 02:47:33 +0100

    Fatura print php
    print php adicionado

--- views/fatura/print.php
+++ views/fatura/print.php
@@ -13,92 +13,95 @@
                         <br>
                         <div class="row">
                             <div class="col-xs-12">
-                                <h2>fatura<br>
-                                    <span class="small">numero #<?= $fatura->id ?></span>
+                                <h2>Fatura
+                                    <span class="small">Nº<?= $fatura->id ?></span>
+                                    <span class="small"> - <i><?= $funcionario->username?></i></span>
                                 </h2>
                             </div>
                         </div>
                     </div>
                     <hr>
                     <div class="row">
-                        <div class="col-xs-6">
-                            <address>
-                                Twitter, Inc.<br>
-                                795 Folsom Ave, Suite 600<br>
-                                San Francisco, CA 94107<br>
-                                <abbr title="Phone">P:</abbr> (123) 456-7890
-                            </address>
+                        <div class="col-sm-6">
+                            <div class="card" style="border-style:hidden;">
+                                <div class="card-body">
+                                    <h5 class="card-title">Cliente</h5>
+                                    <b>Nome: </b><?= $cliente->username ?><br>
+                                    <b>Email: </b><?= $cliente->email ?><br>
+                                    <b>Telefone: </b><?= $cliente->telefone ?><br>
+                                    <b>NIF: </b><?= $cliente->nif ?><br>
+                                    <b>Morada: </b><?= $cliente->morada ?><br>
+                                    <b>Código Postal: </b><?= $cliente->cod_postal ?><br>
+                                    <b>Localidade: </b><?= $cliente->localidade ?><br>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-xs-6 text-right">
-                            <address>
-                                Elaine Hernandez<br>
-                                P. Sherman 42,<br>
-                                Wallaby Way, Sidney<br>
-                                <abbr title="Phone">P:</abbr> (123) 345-6789
-                            </address>
+                        <div class="col-sm-6">
+                            <div class="card" style="border-style:hidden;">
+                                <div class="card-body">
+                                    <h5 class="card-title">Empresa</h5>
+                                    <b>Nome: </b><?= $empresa->nome ?><br>
+                                    <b>Email: </b><?= $empresa->email ?><br>
+                                    <b>Telefone: </b><?= $empresa->telefone ?><br>
+                                    <b>NIF: </b><?= $empresa->nif ?><br>
+                                    <b>Morada: </b><?= $empresa->morada ?><br>
+                                    <b>Código Postal: </b><?= $empresa->cod_postal ?><br>
+                                    <b>Localidade: </b><?= $empresa->localidade ?><br>
+                                    <b>Capital Social: </b><?= $empresa->capital_social?>€<br>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-xs-6 text-right">
-                            <address>
-                                <strong>Order Date:</strong><br>
-                                17/06/14
-                            </address>
+                            <br>
+                            <strong>Data de Emissão: </strong><br><?= $fatura->data->format('d-M-Y') ?>
+                            <br>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-12">
-                            <h3>ORDER SUMMARY</h3>
-                            <table class="table table-striped">
+                            <h3>Linhas Fatura</h3>
+                            <table class="table">
                                 <thead>
                                     <tr class="line">
-                                        <td><strong>#</strong></td>
-                                        <td class="text-center"><strong>PROJECT</strong></td>
-                                        <td class="text-center"><strong>HRS</strong></td>
-                                        <td class="text-right"><strong>RATE</strong></td>
-                                        <td class="text-right"><strong>SUBTOTAL</strong></td>
+                                        <td><strong>Referência</strong></td>
+                                        <td class="text-right"><strong>Nome</strong></td>
+                                        <td class="text-right"><strong>Quantidade</strong></td>
+                                        <td class="text-right"><strong>IVA</strong></td>
+                                        <td class="text-right"><strong>Preço</strong></td>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <tr>
-                                        <td>1</td>
-                                        <td><strong>Template Design</strong><br>A website template is a pre-designed webpage, or set of webpages, that anyone can modify with their own content and images to setup a website.</td>
-                                        <td class="text-center">15</td>
-                                        <td class="text-center">$75</td>
-                                        <td class="text-right">$1,125.00</td>
+                                        <?php foreach ($linhas as $linha) {
+                                            $produto = Produto::find([$linha->produto_id]);
+                                        ?>
+                                            <td><?= $produto->referencia ?></td>
+                                            <td><?= $produto->descricao ?><br></td>                  
+                                            <td class="text-right"><?= $linha->quantidade ?></td>
+                                            <td class="text-right"><?= $iva[array_search($produto->iva_id, array_column($iva, 'id'))]->percentagem?>%</td>
+                                            <td class="text-right"><?= $linha->valor_uni * $linha->quantidade ?>€</td>
                                     </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td><strong>Template Development</strong><br>Web development is a broad term for the work involved in developing a web site for the Internet (World Wide Web) or an intranet (a private network).</td>
-                                        <td class="text-center">15</td>
-                                        <td class="text-center">$75</td>
-                                        <td class="text-right">$1,125.00</td>
-                                    </tr>
-                                    <tr class="line">
-                                        <td>3</td>
-                                        <td><strong>Testing</strong><br>Take measures to check the quality, performance, or reliability of (something), especially before putting it into widespread use or practice.</td>
-                                        <td class="text-center">2</td>
-                                        <td class="text-center">$75</td>
-                                        <td class="text-right">$150.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td colspan="3"></td>
-                                        <td class="text-right"><strong>Taxes</strong></td>
-                                        <td class="text-right"><strong>N/A</strong></td>
-                                    </tr>
-                                    <tr>
-                                        <td colspan="3">
-                                        </td>
-                                        <td class="text-right"><strong>Total</strong></td>
-                                        <td class="text-right"><strong>$2,400.00</strong></td>
-                                    </tr>
+                                <?php } ?>
+                                <tr>
+                                    <td colspan="3"></td>
+                                    <td class="text-right"><strong>Subtotal (Sem IVA)</strong></td>
+                                    <td class="text-right"><?= $fatura->valor_preco_total ?>€</td>
+                                </tr>
+                                <tr>
+                                    <td colspan="3"></td>
+                                    <td class="text-right"><strong>IVA</strong></td>
+                                    <td class="text-right"><?= $fatura->valor_iva_total ?>€</td>
+                                </tr>
+                                <tr>
+                                    <td colspan="3">
+                                    </td>
+                                    <td class="text-right"><strong>Total</strong></td>
+                                    <td class="text-right"><?= $fatura->valor_preco_total + $fatura->valor_iva_total ?>€</td>
+                                </tr>
                                 </tbody>
                             </table>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12 text-right identity">
-                            <p>Designer identity<br><strong>Jeffrey Williams</strong></p>
                         </div>
                     </div>
                 </div>
